121e6a54594e1cf41edbe2a8feb4236bdc9f410d
Diego HR <dhernandez2448@gmail.com>
2023-04-25 18:35:29 -0400

    added whiteboard demos using iframe elements to link to vercel react app

--- javascripts/index.js
+++ javascripts/index.js
@@ -146,3 +146,17 @@ const handlePageLoad = () => {
 };
 
 document.addEventListener("DOMContentLoaded", handlePageLoad);
+
+const REACT_SERVER = 'https://symphony-demo.vercel.app/';
+
+window.onload = () => {
+  const randRoom = () => `${REACT_SERVER}${Math.floor(Math.random() * 10000)}`;
+  
+  const singleWhiteboardFrame = document.getElementById('singleplayer-demo').children[0];
+  const multiWhiteboardFrames = document.getElementById('multiplayer-demo').children;
+
+  singleWhiteboardFrame.setAttribute('src', randRoom());
+
+  const multiplayerDemoRoom = randRoom();
+  [...multiWhiteboardFrames].forEach(frame => frame.src = multiplayerDemoRoom)
+}
